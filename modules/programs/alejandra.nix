52bd56db74a091b412865d52ffc73e4eb94b684b
vai <vai.sriv@icloud.com>
2025-02-03 17:56:13 -0500

    alejandra: fixup lib issues

--- modules/programs/alejandra.nix
+++ modules/programs/alejandra.nix
@@ -1,53 +1,48 @@
 {
-	config,
-	lib,
-	pkgs,
-	...
+  config,
+  lib,
+  pkgs,
+  ...
 }: let
-	cfg = config.programs.alejandra;
-	tomlFormat = pkgs.formats.toml {};
+  cfg = config.programs.alejandra;
+  tomlFormat = pkgs.formats.toml {};
 in {
-	meta.maintainers = [lib.hm.maintainers.vaisriv];
+  meta.maintainers = [lib.hm.maintainers.vaisriv];
 
-	options.programs.alejandra = {
-		enable = lib.mkEnableOption "alejandra";
+  options.programs.alejandra = {
+    enable = lib.mkEnableOption "alejandra";
 
-		package =
-			mkOption {
-				type = types.package;
-				default = pkgs.alejandra;
-				defaultText = literalExpression "pkgs.alejandra";
-				description = ''
-				  Alejandra package to install.
-				'';
-			};
+    package = lib.mkPackageOption {
+      type = types.package;
+      default = pkgs.alejandra;
+      defaultText = lib.literalExpression "pkgs.alejandra";
+      description = ''
+        Alejandra package to install.
+      '';
+    };
 
-		config =
-			lib.mkOption {
-				type = tomlFormat.type;
-				default = {};
-				example =
-					lib.literalExpression ''
-					  # (experimental) Configuration options for Alejandra
+    config = lib.mkOption {
+      type = tomlFormat.type;
+      default = {};
+      example = lib.literalExpression ''
+        # (experimental) Configuration options for Alejandra
 
-					  indentation = "Tabs" # Or: FourSpaces, Tabs
-					'';
-				description = ''
-				  Configuration written to {file}`$XDG_CONFIG_HOME/alejandra/config.toml`.
+        indentation = "Tabs" # Or: FourSpaces, Tabs
+      '';
+      description = ''
+        Configuration written to {file}`$XDG_CONFIG_HOME/alejandra/config.toml`.
 
-				  See <https://github.com/kamadorueda/alejandra/blob/main/alejandra.toml> for
-				  available options and documentation.
-				'';
-			};
-	};
+        See <https://github.com/kamadorueda/alejandra/blob/main/alejandra.toml> for
+        available options and documentation.
+      '';
+    };
+  };
 
-	config =
-		lib.mkIf cfg.enable {
-			home.packages = [cfg.package];
+  config = lib.mkIf cfg.enable {
+    home.packages = [cfg.package];
 
-			xdg.configFile."alejandra/config.toml" =
-				lib.mkIf (cfg.settings != {}) {
-					source = tomlFormat.generate "config.toml" cfg.settings;
-				};
-		};
+    xdg.configFile."alejandra/config.toml" = lib.mkIf (cfg.settings != {}) {
+      source = tomlFormat.generate "config.toml" cfg.settings;
+    };
+  };
 }
